98b77c23c2da11e6941c9462a31496bce1d26bdf
Hassan Mustafa <hassanmustafa030107@gmail.com>
2025-03-29 01:15:31 +0800

    pseudocode for program 10

---
+++ manual_title_case_without_title_function.py
@@ -0,0 +1,4 @@
+#Prog10. title() makes all first letter of each word in the string, capital letter. And all other letter in small case. Create a program that do the same functionality without using title() function.
+#input text "I LOVE RADIOHEAD"
+#like in program 1, use split to split the words and capitalize the first letter and lower the others
+#print "I Love Radiohead"
