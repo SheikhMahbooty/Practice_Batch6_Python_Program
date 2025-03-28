0a03a07362f5265a47356ff0c3c1ebbf82f954f2
Hassan Mustafa <hassanmustafa030107@gmail.com>
2025-03-28 23:50:58 +0800

    Created code for program 3 without using the lower() function

---
+++ decapitalize_text_without_lower_function.py
@@ -0,0 +1,14 @@
+#Prog03. lower() converts all characters of the string into lower case. Create a program that do the same functionality without using lower() function.
+
+#Input text "I LOVE RADIOHEAD"
+text = input("Enter text: ")
+
+#Manual listing of characters"
+uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+lowercase = "abcdefghijklmnopqrstuvwxyz"
+
+#Transform characters to lowercase
+result = text.translate(str.maketrans(uppercase, lowercase))
+
+#Print result "i love radiohead"
+print("Lowercase:", result)
