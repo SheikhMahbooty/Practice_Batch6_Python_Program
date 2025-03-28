48e08c30591a1795ffca74f6c6755e83d1480f03
Hassan Mustafa <hassanmustafa030107@gmail.com>
2025-03-29 01:02:26 +0800

    Fixed variables, comments, and tweaked the texts

--- swap_capitalization_without_swapcase_function.py
+++ swap_capitalization_without_swapcase_function.py
@@ -1,7 +1,7 @@
 #Prog08. swapcase() reverse the casing of each of the character of the string. Create a program that do the same functionality without using swapcase() function.
 
 #input text "i DONT lOvE rAdIoHEAD"
-text = input("Enter text: ")
+input_text = input("Enter text: ")
 result = ""
 
 #manually add characters a-z
@@ -9,8 +9,8 @@ lowercase = "abcdefghijklmnopqrstuvwxyz"
 uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 #for loop
-for char in text:
-    if char in lowercase:
+for character in input_text:
+    if character in lowercase:
         result += uppercase[lowercase.index(char)]
     elif char in uppercase:
         result += lowercase[uppercase.index(char)]
@@ -18,5 +18,5 @@ for char in text:
         result += char
         
 #print "I dont LoVe RaDiOhead"
-print("Swapcase result:", result)
+print("Swapped Capitalization:", result)
 
