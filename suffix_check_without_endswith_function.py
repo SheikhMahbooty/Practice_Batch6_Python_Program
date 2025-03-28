9cb8e22b19d625a3c70fcbc588010b642e4cc6ef
Hassan Mustafa <hassanmustafa030107@gmail.com>
2025-03-29 00:33:43 +0800

    initial psuedocode for program 5

---
+++ suffix_check_without_endswith_function.py
@@ -0,0 +1,4 @@
+#Prog05. endswith() check if the string end part matches the function parameter. Create a program that do the same functionality without using endswith() function.
+#input text "i love radiohead"
+#check the suffix
+#check if suffix ends with "radiohead"
