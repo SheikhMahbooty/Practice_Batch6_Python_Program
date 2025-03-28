1a024eeff3727757f6269f2e0443882ce225ab05
Hassan Mustafa <hassanmustafa030107@gmail.com>
2025-03-28 23:38:36 +0800

    Created program 1 that uses the split function

--- remove_spaces_without_lstrip_function.py
+++ remove_spaces_without_lstrip_function.py
@@ -1,3 +1,9 @@
-#Prog01. lstrip() remove the space characters at the beginning of the string. Create a program that do the same functionality without using lstrip() function.
-#input "   i LOVE radiohead"
-#print "i LOVE radiohead" without lstrip() function
+#Prog01. lstrip() remove the space characters at the beginning of the string. Create a program that do the same functionality without using lstrip() function
+
+input_text = input("Enter text: ")
+
+#Split the text and use the first split and remove the leading spaces there
+index = input_text.find(text.split()[0])
+result = input_text[index:]
+
+print("Without leading spaces:", result)
