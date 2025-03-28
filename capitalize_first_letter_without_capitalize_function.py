e9e87d631cac48d3537d45d840a7b8b3af30b7bc
Hassan Mustafa <hassanmustafa030107@gmail.com>
2025-03-29 01:09:33 +0800

    Fixed variable names and comments

--- capitalize_first_letter_without_capitalize_function.py
+++ capitalize_first_letter_without_capitalize_function.py
@@ -1,12 +1,12 @@
 #Prog09. capitalize() makes the first letter of the string, capital letter. And all other letter in small case. Create a program that do the same functionality without using capitalize() function.
 #Input text " rADIOHEAD"
-text = input("Enter text: ")
+input_text = input("Enter text: ")
 result = ""
 
 #Check if text has upper and lower values and capitalize the first letter of the string
-if text:
-    result += text[0].upper()
-    result += ''.join(c.lower() for c in text[1:])
+if input_text:
+    result += input_text[0].upper()
+    result += ''.join(char.lower() for char in text[1:])
 
 #print text "Radiohead"
 print("Capitalized:", result)
