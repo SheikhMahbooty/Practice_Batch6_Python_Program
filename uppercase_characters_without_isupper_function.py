d7d1d2da8215a123744c3db5362298c2e88f9ed5
Hassan Mustafa <hassanmustafa030107@gmail.com>
2025-03-29 00:26:01 +0800

    Created code for program 4 that checks if the text is an uppercase text

--- uppercase_characters_without_isupper_function.py
+++ uppercase_characters_without_isupper_function.py
@@ -1,4 +1,15 @@
 #Prog04. isupper() check if all characters of the string is on upper case. Create a program that do the same functionality without using isupper() function.
 #input text I LOVE RADIOHEAD
-#check if all characters are uppercase
-#print True
+text = input("Enter text: ")
+
+#Initial value
+all_upper = True 
+
+#Check if theres any lowercase characters
+for char in text:
+    if 'a' <= char <= 'z':  
+        all_upper = False
+        break
+
+#Print True if all characters are uppercase
+print("Are all characters uppercase:", all_upper)
