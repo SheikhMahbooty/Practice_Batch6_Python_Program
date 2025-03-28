59eebfc4c572177c6c5f2aad3ae64c9ec2d72901
Hassan Mustafa <hassanmustafa030107@gmail.com>
2025-03-29 01:08:19 +0800

    Created code for program 9 that uses the lower and upper functions to manually verify the capitalization of each character

--- capitalize_first_letter_without_capitalize_function.py
+++ capitalize_first_letter_without_capitalize_function.py
@@ -1,3 +1,12 @@
 #Prog09. capitalize() makes the first letter of the string, capital letter. And all other letter in small case. Create a program that do the same functionality without using capitalize() function.
 #Input text " rADIOHEAD"
-#capitalized text without capitalize function "Radiohead"
+text = input("Enter text: ")
+result = ""
+
+#Check if text has upper and lower values and capitalize the first letter of the string
+if text:
+    result += text[0].upper()
+    result += ''.join(c.lower() for c in text[1:])
+
+#print text "Radiohead"
+print("Capitalized:", result)
