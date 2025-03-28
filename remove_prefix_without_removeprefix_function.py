27107c555a82cf7bee43eb005ce313eacfe79980
Hassan Mustafa <hassanmustafa030107@gmail.com>
2025-03-28 23:42:15 +0800

    pseudocode for program 2

---
+++ remove_prefix_without_removeprefix_function.py
@@ -0,0 +1,4 @@
+#Prog02. removeprefix() remove the characters at the beginning of the string that matches the function parameter. Create a program that do the same functionality without using removeprefix() function.
+#input text
+#loop for removing a prefix without removeprefix()
+#print output
