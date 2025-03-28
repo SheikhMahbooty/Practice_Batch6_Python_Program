d73df4fb8fa94aa12a4e3cd9774c3ff91c9cf0f1
Hassan Mustafa <hassanmustafa030107@gmail.com>
2025-03-29 01:16:33 +0800

    Created code for program 10 that uses split to categorize the words and capitalize the first letter and the others to lower case

--- manual_title_case_without_title_function.py
+++ manual_title_case_without_title_function.py
@@ -1,4 +1,19 @@
 #Prog10. title() makes all first letter of each word in the string, capital letter. And all other letter in small case. Create a program that do the same functionality without using title() function.
+
 #input text "I LOVE RADIOHEAD"
+text = input("Enter text: ")
+
+words = text.split()
+result = []
+
 #like in program 1, use split to split the words and capitalize the first letter and lower the others
+for word in words:
+    if word:
+        new_word = word[0].upper() + word[1:].lower()
+        result.append(new_word)
+
+final = " ".join(result)
+
 #print "I Love Radiohead"
+print("Title case:", final)
+
