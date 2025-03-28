1e7e39866959f317f24888fe23119b56cb5a9746
Hassan Mustafa <hassanmustafa030107@gmail.com>
2025-03-29 00:35:27 +0800

    Created code for program 5 that checks the suffix and values it if it is true or false

--- suffix_check_without_endswith_function.py
+++ suffix_check_without_endswith_function.py
@@ -1,4 +1,10 @@
 #Prog05. endswith() check if the string end part matches the function parameter. Create a program that do the same functionality without using endswith() function.
 #input text "i love radiohead"
-#check the suffix
-#check if suffix ends with "radiohead"
+text = input("Enter text: ")
+suffix = input("Enter ending text to check: ")
+
+#Input suffix to verify
+result = text[len(text) - len(suffix):] == suffix
+
+#Verify if suffix ends with "radiohead"
+print("Ends with suffix:", result)
