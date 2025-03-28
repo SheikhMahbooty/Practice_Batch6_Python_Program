f83754adf2860a637565528f2afc87a6b391ab32
Hassan Mustafa <hassanmustafa030107@gmail.com>
2025-03-29 01:00:03 +0800

    pseudocode for program 8

---
+++ swap_capitalization_without_swapcase_function.py
@@ -0,0 +1,5 @@
+#Prog08. swapcase() reverse the casing of each of the character of the string. Create a program that do the same functionality without using swapcase() function.
+#input text "i DONT lOvE rAdIoHEAD"
+#manually add characters a-z
+#for loop
+#print "I dont LoVe RaDiOhead"
