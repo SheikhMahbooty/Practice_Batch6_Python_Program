57efb51f6364b95db1ca646a47b352783afd628d
Hassan Mustafa <hassanmustafa030107@gmail.com>
2025-03-29 15:30:34 +0800

    Fixed variable names for characters

--- count_characters_without_using_count_function.py
+++ count_characters_without_using_count_function.py
@@ -1,8 +1,9 @@
 #Prog08. count() return how many time the function parameter appear in the string. Create a program that do the same functionality without using count() function.
+
 #input text "i am thom yorke"
-text = input("Text: ")
-char = input("Char: ") #input character to count "o"
+input_text = input("Text: ")
+selected_char = input("Char: ") #input character to count "o"
 
 #print how many times the character appeared "o" = 2
-print(sum(1 for c in text if c == char))
+print(sum(1 for characters in input_text if characters == selected_char))
 
