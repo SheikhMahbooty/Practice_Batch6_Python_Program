f7426a94bcd99afe7c06face91d58be78a0b8696
Hassan Mustafa <hassanmustafa030107@gmail.com>
2025-03-29 15:29:28 +0800

    Created code for program 8 that counts characters without count function

--- count_characters_without_using_count_function.py
+++ count_characters_without_using_count_function.py
@@ -1,4 +1,8 @@
 #Prog08. count() return how many time the function parameter appear in the string. Create a program that do the same functionality without using count() function.
 #input text "i am thom yorke"
-#input character "o"
-#print how many times "o" appeared = 2
+text = input("Text: ")
+char = input("Char: ") #input character to count "o"
+
+#print how many times the character appeared "o" = 2
+print(sum(1 for c in text if c == char))
+
