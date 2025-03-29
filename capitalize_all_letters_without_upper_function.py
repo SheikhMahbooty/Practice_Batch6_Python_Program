be8e673923a2979d3e4c4df2f25f158f84016dbe
Hassan Mustafa <hassanmustafa030107@gmail.com>
2025-03-29 14:16:20 +0800

    pseudocode for program 3

---
+++ capitalize_all_letters_without_upper_function.py
@@ -0,0 +1,4 @@
+#Prog03. upper() converts all characters of the string into upper case. Create a program that do the same functionality without using upper() function.
+#input text "i love radiohead"
+#change text to uppercase using manual characters
+#print "I LOVE RADIOHEAD"
