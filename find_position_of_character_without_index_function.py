f00b0fbd6377e3cce81d41ed3b178a43b7b4f2b5
Hassan Mustafa <hassanmustafa030107@gmail.com>
2025-03-29 15:33:29 +0800

    pseudocode for program 9

---
+++ find_position_of_character_without_index_function.py
@@ -0,0 +1,5 @@
+#Prog09. index() return the first location of the function parameter in the string. Create a program that do the same functionality without using index() function.
+#input text "thom yorke"
+#input character "o"
+#check what position the characters are
+#print what position it is "2"
