8edbe7ac6107868d8421fb6daeca575da4f0360d
Hassan Mustafa <hassanmustafa030107@gmail.com>
2025-03-29 15:42:07 +0800

    Created code for program 10 that finds the last position of the character without using rindex

--- find_last_position_of_character_without_rindex_function.py
+++ find_last_position_of_character_without_rindex_function.py
@@ -1,5 +1,12 @@
 #Prog10. rindex() return the first location of the function parameter in the string starting from the last character. Create a program that do the same functionality without using rindex() function.
 #input text "thom yorke"
-#input character "o"
-#find where the character appears last "6"
-#print the index
+text = input("Enter text: ")
+char = input("Enter character: ") #input character "o"
+
+for i in range(len(text) - 1, -1, -1):
+    if text[i] == char:
+        print(f"Last '{char}' at index {i}")
+        break
+else:
+    print(f"'{char}' not found.")
+
