2025280b9491de143bfab7c438a9f15437780b37
Hassan Mustafa <hassanmustafa030107@gmail.com>
2025-03-29 15:03:07 +0800

    Fixed spaces and variable names

--- add_spaces_in_beginning_without_rjust.py
+++ add_spaces_in_beginning_without_rjust.py
@@ -1,10 +1,11 @@
 #Prog06. rjust() add space characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using rjust() function.
-#input text "i love radiohead"
-text = input("Enter text: ")
-width = 10 #initial value
 
-spaces = " " * (width - len(text))
-result = spaces + text
+#input text "i love radiohead"
+input_text = input("Enter text: ")
+text_width = 10 #initial value
+
+spaces = " " * (text_width - len(input_text))
+result = spaces + input_text
 
 #print the result "   i love radiohead"
 print(f"'{result}'")
