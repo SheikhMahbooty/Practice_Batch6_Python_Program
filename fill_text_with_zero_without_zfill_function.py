70858216bd4bb24f4b42cd1eda33683258b64571
Hassan Mustafa <hassanmustafa030107@gmail.com>
2025-03-29 15:21:09 +0800

    Fixed variable names and modified comments

--- fill_text_with_zero_without_zfill_function.py
+++ fill_text_with_zero_without_zfill_function.py
@@ -1,14 +1,15 @@
 #Prog07. zfill() add zero characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using zfill() function.
 
-#input text "123"
-text = input("Enter number/s: ")
-width = int(input("Enter total width: "))
+#input text "69420"
+input_text = input("Enter number/s: ")
+input_width = int(input("Enter total width: ")) #add desired zeros "10"
 
 #add the remaining zeros missing from the inputted width
-if width > len(text):
-    zeros = "0" * (width - len(text))
-    result = zeros + text
+if input_width > len(input_text):
+    zero_count = "0" * (input_width - len(input_text))
+    result = zero_count + input_text
 else:
-    result = text
+    result = input_text
 
+#print output "0000069420"
 print(result)
