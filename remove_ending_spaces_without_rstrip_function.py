9d3f4ca13163d0987f0879d5f0ff6730b0a8bcb5
Hassan Mustafa <hassanmustafa030107@gmail.com>
2025-03-29 14:04:25 +0800

    Actual pseudocode for program 1 since I uploaded the wrong save file

--- remove_ending_spaces_without_rstrip_function.py
+++ remove_ending_spaces_without_rstrip_function.py
@@ -1,6 +1,4 @@
 #Prog01. rstrip() remove the space characters at the end of the string. Create a program that do the same functionality without using rstrip() function.
-
 #input text "i love radiohead     "
-input_text = input("Type your text: ")
-result = ' '.join(input_text.split())
-print(f"'{result}'")
+#use split to get the last part of the text and remove the spaces
+#print "i love radiohead"
