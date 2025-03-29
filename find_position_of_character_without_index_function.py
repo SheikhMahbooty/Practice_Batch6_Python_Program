34d1b691d7d1e4ef30ec3f539b31a875477fe7f0
Hassan Mustafa <hassanmustafa030107@gmail.com>
2025-03-29 15:34:25 +0800

    Created code for program 9 that finds the index of the character without using the index function

--- find_position_of_character_without_index_function.py
+++ find_position_of_character_without_index_function.py
@@ -1,5 +1,15 @@
 #Prog09. index() return the first location of the function parameter in the string. Create a program that do the same functionality without using index() function.
 #input text "thom yorke"
-#input character "o"
-#check what position the characters are
-#print what position it is "2"
+text = input("Enter a text: ")
+char = input("Enter character to find: ") #input character "o"
+
+position = -1
+for i in range(len(text)):
+    if text[i] == char:
+        position = i
+        break
+
+if position != -1:
+    print(f"First occurrence at index {position}")
+else:
+    print("Character not found.")
