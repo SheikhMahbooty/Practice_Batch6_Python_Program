9f9438aafbb55fb3c5a4314944e0ba3ac6a714c7
Hassan Mustafa <hassanmustafa030107@gmail.com>
2025-03-29 14:12:51 +0800

    pseudocode for program 2

---
+++ remove_characters_in_suffix_without_removesuffix.py
@@ -0,0 +1,4 @@
+#Prog02. removesuffix() remove the characters at the end of the string that matches the function parameter. Create a program that do the same functionality without using removesuffix() function.
+#input text "i love radiohead"
+#remove the text "radiohead"
+#print "i love"
