8f9a210dfc2a65ed281032ba66ffee06883531d0
Hassan Mustafa <hassanmustafa030107@gmail.com>
2025-03-29 14:19:03 +0800

    Created code for program 3 that capitalizes all characters without using upper function

--- capitalize_all_letters_without_upper_function.py
+++ capitalize_all_letters_without_upper_function.py
@@ -1,4 +1,20 @@
 #Prog03. upper() converts all characters of the string into upper case. Create a program that do the same functionality without using upper() function.
+
 #input text "i love radiohead"
-#change text to uppercase using manual characters
+input_text = input("Enter text: ")
+lowercase = "abcdefghijklmnopqrstuvwxyz"
+uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+#initial value
+result = ""
+
+#change text from lower to uppercase
+for char in input_text:
+    if char in lowercase:
+        index = lowercase.index(char)
+        result += uppercase[index]
+    else:
+        result += char
+
 #print "I LOVE RADIOHEAD"
+print(result)
