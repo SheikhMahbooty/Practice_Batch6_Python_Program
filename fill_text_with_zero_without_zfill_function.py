c96056c5dd48f5f06d99d82a560e4450a9eda8d2
Hassan Mustafa <hassanmustafa030107@gmail.com>
2025-03-29 15:13:47 +0800

    Created code for program 7 that completes the number of inputted number of digits without using zfill function

--- fill_text_with_zero_without_zfill_function.py
+++ fill_text_with_zero_without_zfill_function.py
@@ -1,4 +1,14 @@
 #Prog07. zfill() add zero characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using zfill() function.
+
 #input text "123"
-#input spaces "5"
-#print "00123"
+text = input("Enter number/s: ")
+width = int(input("Enter total width: "))
+
+#add the remaining zeros missing from the inputted width
+if width > len(text):
+    zeros = "0" * (width - len(text))
+    result = zeros + text
+else:
+    result = text
+
+print(result)
