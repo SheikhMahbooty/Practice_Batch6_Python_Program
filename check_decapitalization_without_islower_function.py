7d248b6c413874c2db303c3af835f9e65f7a7214
Hassan Mustafa <hassanmustafa030107@gmail.com>
2025-03-29 14:39:37 +0800

    created pseudocode for program 4

---
+++ check_decapitalization_without_islower_function.py
@@ -0,0 +1,5 @@
+#Prog04. islower() check if all characters of the string is on lower case. Create a program that do the same functionality without using islower() function.
+#input text "i love radiohead"
+#change text to uppercase using manual characters
+#print "I LOVE RADIOHEAD"
+
