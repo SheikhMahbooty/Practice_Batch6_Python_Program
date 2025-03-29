58b2d2cb9735ff332c43e687c671f5815e9a09e6
Hassan Mustafa <hassanmustafa030107@gmail.com>
2025-03-29 14:42:59 +0800

    Created code for program 4 that checks if all the characters are in lower case

--- check_decapitalization_without_islower_function.py
+++ check_decapitalization_without_islower_function.py
@@ -1,5 +1,17 @@
 #Prog04. islower() check if all characters of the string is on lower case. Create a program that do the same functionality without using islower() function.
 #input text "i love radiohead"
-#change text to uppercase using manual characters
+input_text = input("Enter text: ")
+uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+all_lower = True
+
+#check text if characters are lowercase
+for char in input_text:
+    if char in uppercase:
+        all_lower = False
+        break
+
 #print "I LOVE RADIOHEAD"
+print(all_lower)
+
 
