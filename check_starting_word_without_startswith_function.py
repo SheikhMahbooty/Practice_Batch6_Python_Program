b61dc1880099b0a7980a77b0c33eef1e3e8c7005
Hassan Mustafa <hassanmustafa030107@gmail.com>
2025-03-29 14:49:22 +0800

    Created base code for program 5 that verifies the suffix of the input

--- check_starting_word_without_startswith_function.py
+++ check_starting_word_without_startswith_function.py
@@ -1,4 +1,8 @@
 #Prog05. startswith() check if the string beginning part matches the function parameter. Create a program that do the same functionality without using startswith() function.
 #input text "radio head"
-#input start text "radio"
-#print "true"
+text = input("Enter text: ")
+prefix = input("Enter prefix: ")
+
+matches = text[:len(prefix)] == prefix
+
+print(matches)
