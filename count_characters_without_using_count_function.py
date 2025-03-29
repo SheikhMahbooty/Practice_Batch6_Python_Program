576561af1a2bf0dc7f75293dd03d937fa23f0ced
Hassan Mustafa <hassanmustafa030107@gmail.com>
2025-03-29 15:28:08 +0800

    pseudocode for program 8

---
+++ count_characters_without_using_count_function.py
@@ -0,0 +1,4 @@
+#Prog08. count() return how many time the function parameter appear in the string. Create a program that do the same functionality without using count() function.
+#input text "i am thom yorke"
+#input character "o"
+#print how many times "o" appeared = 2
