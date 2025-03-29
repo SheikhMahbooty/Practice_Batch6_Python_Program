d5f80de8ae6960a222119123159d7d39a1357e8f
Hassan Mustafa <hassanmustafa030107@gmail.com>
2025-03-29 14:50:16 +0800

    Added comments

--- check_starting_word_without_startswith_function.py
+++ check_starting_word_without_startswith_function.py
@@ -1,8 +1,10 @@
 #Prog05. startswith() check if the string beginning part matches the function parameter. Create a program that do the same functionality without using startswith() function.
 #input text "radio head"
 text = input("Enter text: ")
-prefix = input("Enter prefix: ")
+prefix = input("Enter prefix: ") #input prefix "radio"
 
+#check if inputted prefix matches the text
 matches = text[:len(prefix)] == prefix
 
+#print "true"
 print(matches)
