d97b876f319b8743e2a040bba88d146ecfa372a6
Hassan Mustafa <hassanmustafa030107@gmail.com>
2025-03-29 15:12:33 +0800

    pseudocode for program 7

---
+++ fill_text_with_zero_without_zfill_function.py
@@ -0,0 +1,4 @@
+#Prog07. zfill() add zero characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using zfill() function.
+#input text "123"
+#input spaces "5"
+#print "00123"
