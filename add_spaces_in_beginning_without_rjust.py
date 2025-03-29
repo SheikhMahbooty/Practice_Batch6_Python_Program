3841f352d20d2086f3eb250f37ae77aa1108ccd8
Hassan Mustafa <hassanmustafa030107@gmail.com>
2025-03-29 15:01:30 +0800

    Fixed comments

--- add_spaces_in_beginning_without_rjust.py
+++ add_spaces_in_beginning_without_rjust.py
@@ -1,9 +1,10 @@
 #Prog06. rjust() add space characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using rjust() function.
 #input text "i love radiohead"
 text = input("Enter text: ")
-width = 10
+width = 10 #initial value
 
 spaces = " " * (width - len(text))
 result = spaces + text
 
+#print the result "   i love radiohead"
 print(f"'{result}'")
