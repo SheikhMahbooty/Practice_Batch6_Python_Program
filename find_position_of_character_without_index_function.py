881a3919621b5a906505a4841c59f8ebd7e2777b
Hassan Mustafa <hassanmustafa030107@gmail.com>
2025-03-29 15:36:30 +0800

    Fixed variables and comments in loop and in code

--- find_position_of_character_without_index_function.py
+++ find_position_of_character_without_index_function.py
@@ -1,14 +1,17 @@
 #Prog09. index() return the first location of the function parameter in the string. Create a program that do the same functionality without using index() function.
-#input text "thom yorke"
-text = input("Enter a text: ")
-char = input("Enter character to find: ") #input character "o"
 
+#input text "thom yorke"
+input_text = input("Enter a text: ")
+chosen_char = input("Enter character to find: ") #input character "o"
+
+#find position
 position = -1
-for i in range(len(text)):
-    if text[i] == char:
-        position = i
+for index in range(len(input_text)):
+    if input_text[index] == chosen_char:
+        position = index
         break
 
+#print where the character is "2"
 if position != -1:
     print(f"First occurrence at index {position}")
 else:
