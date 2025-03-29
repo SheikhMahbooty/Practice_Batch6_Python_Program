47a75dcd829aab8a6ee56bdbd2265dd8d456ae2d
Hassan Mustafa <hassanmustafa030107@gmail.com>
2025-03-29 14:13:29 +0800

    Created code for program 1 which removes the selected suffix without using removesuffix function

--- remove_characters_in_suffix_without_removesuffix.py
+++ remove_characters_in_suffix_without_removesuffix.py
@@ -1,4 +1,12 @@
 #Prog02. removesuffix() remove the characters at the end of the string that matches the function parameter. Create a program that do the same functionality without using removesuffix() function.
+
 #input text "i love radiohead"
-#remove the text "radiohead"
-#print "i love"
+input_text = input("Enter text: ")
+suffix = input("Enter suffix to remove: ")
+
+#remove the desired suffix "head"
+if input_text.endswith(suffix):
+    input_text = input_text[:len(input_text) - len(suffix)]
+
+#print "i love radio"
+print(input_text)
